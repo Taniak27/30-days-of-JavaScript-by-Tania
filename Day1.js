25213e227b2445e5c0476a54922d9896143a150d
Taniak27 <127733847+Taniak27@users.noreply.github.com>
2023-03-22 17:38:30 +0200

    Adding of day#1

---
+++ Day1.js
@@ -0,0 +1,56 @@
+//TASK_1
+//comments can make code readable
+
+//TASK_2
+//Welcome to 30DaysOfJavaScript
+
+//TASK_3
+/*
+comments can make code readable, easy to reuse and informative
+*/
+
+//Task_4
+let text = 'Hello world'
+let bool = true
+let und = ''
+let zero = null
+console.log(text, bool, und, zero)
+
+//TASK_5
+console.log(typeof 'Tania')
+console.log(typeof 20)
+console.log(typeof false)
+console.log(typeof null)
+console.log(typeof undefined)
+
+//TASK_6
+let firstVar
+let secondVar
+let thirdVar
+let forthVar
+console.log(firstVar, secondVar, thirdVar, forthVar)
+
+//TASK_7
+let name = 'Tania'
+let surname = 'Kuzmenko'
+let age = 27
+let city = 'Odesa'
+console.log(name, surname, age, city)
+
+//TASK_8
+let myName = 'Tatiana'
+let mySurname = 'Kuzmenko'
+let maritalStatus = 'not married'
+let myCountry = 'Ukraine'
+let age = 27;
+console.log(myName, mySurname, maritalStatus, myCountry, age)
+
+//TASK_9
+let myName = 'Tatiana', mySurname = 'Kuzmenko', maritalStatus = 'not married', myCountry = 'Ukraine', age = 27
+
+console.log(myName, mySurname, maritalStatus, myCountry, age)
+
+//TASK_10
+let myAge = 27
+let yourAge = 30
+console.log(myAge, yourAge)
