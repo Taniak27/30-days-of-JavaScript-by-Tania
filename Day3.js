03bf57fc9b020510b88112432fea2574a1a296af
Taniak27 <127733847+Taniak27@users.noreply.github.com>
2023-03-23 16:59:41 +0200

    Adding of the second part of this day

--- Day3.js
+++ Day3.js
@@ -163,3 +163,198 @@ console.log(prb10);
 let prb11 = 'dragon and python'
 console.log(!prb11.includes('on'));
 // false
+
+//Task_7
+const now = new Date()
+console.log(now.getFullYear());
+// 2023
+
+const now2 = new Date()
+console.log(now2.getMonth());
+// 2
+
+const now3 = new Date()
+console.log(now3.getDate());
+// 23
+
+const now4 = new Date()
+console.log(now4.getDay());
+// 4
+
+const now5 = new Date()
+console.log(now5.getHours());
+// 12
+
+const now6 = new Date()
+console.log(now6.getMinutes());
+// 7
+
+const now7 = new Date()
+console.log(now7.getMinutes());
+
+var now8 = new Date();
+var dateDay = new Date(1970, 1, 1);
+var elapsedT = now8 - dateDay; 
+console.log(elapsedT);
+// 1676898768793
+
+//LEVEL_2
+
+//Task_1
+
+let b = 20
+let h = 10
+const areaOfTriangle = 0.5 * b * h
+
+console.log(areaOfTriangle);
+// 100
+
+
+//Task_2
+
+let a = 5
+let b1 = 4
+let c = 3
+const perimeter = a + b1 + c
+
+console.log(perimeter);
+// 12
+
+//Task_3
+
+let length = parseInt(prompt('Enter the length of rectangle')); 
+let width = parseInt(prompt('Enter the width of rectangle')); 
+const areaOfRectangle = length * width;
+
+alert(areaOfRectangle);
+
+
+let length = parseInt(prompt('Enter the length of rectangle')); 
+let breadth = parseInt(prompt('Enter the breadth of rectangle')); 
+const perimeterOfRectangle = length + breadth + length + breadth;
+
+alert(perimeterOfRectangle);
+
+
+//Task_4
+
+const pi = 3.14;
+let r = parseInt(prompt('Enter the radius of circle'));
+const areaOfCircle = pi * r * r;
+
+alert(Math.floor(areaOfCircle));
+
+
+//Task_5
+
+let x = parseInt(prompt('Enter the slope of circle'))
+const slope = x * x - 2
+
+alert(slope);
+
+
+//Task_6
+
+let y1 = parseInt(prompt('Enter the y1 of circle'));
+let y2 = parseInt(prompt('Enter the y2 of circle'));
+let x1 = parseInt(prompt('Enter the x1 of circle'));
+let x2 = parseInt(prompt('Enter the x2 of circle'))
+const m = (y2 - y1) / (x2 - x1)
+
+alert(m);
+
+
+//Task_7
+
+console.log(slope === m);
+// false
+
+//Task_8
+
+let x = parseInt(prompt('Enter the x'))
+const y = x * x + 6 * x + 9
+
+alert(y);
+
+
+//Task_9
+
+let ratePerHour = parseInt(prompt('Enter your rate per hour, please'));
+let hours = parseInt(prompt('Enter your working hours, please'));
+const weeklyEarning = ratePerHour * hours;
+
+alert(weeklyEarning);
+
+
+//Task_10
+
+let myName = 'Tatiana';
+let lengthOfMyName = myName.length;
+lengthOfMyName >= 7
+  ? console.log('You have a long name')
+  : console.log('Your name is short')
+
+// You have a long name
+
+// Task_11
+
+let myFirstName = "Tania"
+let myLastName = "Kuzmenko"
+
+myFirstName.length > myLastName
+  ? console.log(`Your first name, ${myFirstName} is longer than your family name, ${myLastName}`)
+  : console.log(`Your family name, ${myLastName} is longer than your first name, ${myFirstName}`)
+ 
+// Your family name, Kuzmenko is longer than your first name, Tania
+
+//Task_12
+
+let myAge = 250
+let yourAge = 25
+
+myAge > yourAge
+  ? console.log('I am 225 years older than you')
+  : console.log('You are older than me')
+
+//I am 225 years older than you
+
+
+//Task_13
+
+let yearOfBirth = parseInt(prompt('Enter the year of your birth'))
+yearOfBirth > 2005
+  ? alert('You are young enough to drive')
+  : alert('You are old enough to drive')
+
+
+//Task_14
+
+let numbersOflife = parseInt(prompt('Enter number of years you live'))
+const secInYear = 31536000
+let yourAgeInSeconds = numbersOflife * secInYear
+
+alert(yourAgeInSeconds);
+
+//Task_15
+
+const nowIs = new Date()
+const yearIs = nowIs.getFullYear()
+const monthIs = nowIs.getMonth()
+const dateIs = nowIs.getDate()
+const hoursIs = nowIs.getHours()
+const minutesIs = nowIs.getMinutes()
+
+console.log(`${yearIs}-${monthIs}-${dateIs}-${hoursIs}:${minutesIs}`);
+console.log(`${dateIs}-${monthIs}-${yearIs}-${hoursIs}:${minutesIs}`);
+console.log(`${dateIs}/${monthIs}/${yearIs}/${hoursIs}:${minutesIs}`);
+// 2023-2-23-15:57
+//23-2-2023-15:57
+//23/2/2023/15:57
+
+
+//LEVEL_3
+
+//Task_1
+
+console.log(`${yearIs}-${monthIs}-${dateIs}-${hoursIs}:${minutesIs}`);
+// 2023-2-23-16:43
