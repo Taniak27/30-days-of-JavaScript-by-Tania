545a6217c41a480d03fe2892701ac41775539b63
Taniak27 <127733847+Taniak27@users.noreply.github.com>
2023-03-27 19:17:13 +0300

    Adding of web_techs.js file

---
+++ web_techs.js
@@ -0,0 +1,10 @@
+//Task_5
+let webTechs = ["html", "css", "javascript", "react", "angular"]
+
+if (webTechs.includes("sass")) {
+  console.log("sass is a preprocessor")
+} else {
+  webTechs.push("sass");
+}
+console.log(webTechs);
+// [ 'html', 'css', 'javascript', 'react', 'angular', 'sass' ]
