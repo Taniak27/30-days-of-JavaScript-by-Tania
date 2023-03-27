9556e2206676941904159b84cb54212c08ad7853
Taniak27 <127733847+Taniak27@users.noreply.github.com>
2023-03-27 19:16:12 +0300

    Adding of countries.js file

---
+++ countries.js
@@ -0,0 +1,10 @@
+//Task_4
+
+let countries = ['Ukraine', 'the USA', 'Spain', 'France', 'Ethiopia'];
+
+if (countries.includes('Ethiopia')) {
+  console.log('ETHIOPIA')
+} else {
+  console.log(countries.push('Ethiopia'));
+}
+// ETHIOPIA
